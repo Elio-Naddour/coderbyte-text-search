ab26b99c21344d68b6b496dfb297895736126b5a
unknown <enaddour961@gmail.com>
2023-11-03 19:08:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useRef, useState ,useCallback} from 'react';
 import './App.css';
 
 function App() {
+  const data=['William Shakespeare (1564-1616) ',  'Jane Austen (1775-1817) ',  'Charles Dickens (1812-1870) ',  'Leo Tolstoy (1828-1910) ',  'George Orwell (1903-1950) ',  'Gabriel García Márquez (1927-2014) ',  'Haruki Murakami (b. 1949)  ',  'J.K. Rowling (b. 1965) ',  'Chimamanda Ngozi Adichie (b. 1977) ',  'Toni Morrison (1931-2019)'];
+  const [list,setList]=useState(data)
+  const inputRef=useRef()
+
+  const search=useCallback(()=>{
+    setList(data?.filter((text)=> {return text.toLocaleLowerCase().includes(inputRef.current.value); }))
+
+  },[inputRef])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={inputRef} onChange={search} className='searchBar'/>
+      <div className='list'>
+      { list?.length > 0 ? list?.map((text)=>{return <div className='item'>{text}</div>})  : <div className='item'>No Results</div> }
+      </div>
     </div>
   );
 }
